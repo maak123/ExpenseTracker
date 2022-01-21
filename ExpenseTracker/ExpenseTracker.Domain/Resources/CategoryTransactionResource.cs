b0dc4f9aa7107af603d5c499c3f842f64028c461
Amila Keerthi <amila.keerthi@inovaitsys.com>
2022-01-21 22:49:43 +0530

    fix

---
+++ ExpenseTracker/ExpenseTracker.Domain/Resources/CategoryTransactionResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Business.Resources
+{
+    public class CategoryTransactionResource
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public double TransactionsTotal { get; set; }
+
+    }
+}
